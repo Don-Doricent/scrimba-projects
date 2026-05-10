aafc68cd386c49c13750522cf044f273958f83b0
Don Doricent <ddoricent97@gmail.com>
2026-05-10 14:49:14 -0400

    added removeFromCart function

--- tracks/js-basics/restaurantOrderingApp/index.js
+++ tracks/js-basics/restaurantOrderingApp/index.js
@@ -1,6 +1,7 @@
 import { menuArray } from "./data.js";
 
 const menuContainer = document.getElementById("menu-container")
+const orderContainer = document.getElementById("order-container")
 
 let cartArray = []
 
@@ -15,7 +16,7 @@ function renderMenu() {
 
             <p>${item.ingredients.join(", ")}</p>
 
-            <h2 class="price">${item.price}</h2>
+            <h2 class="price">$${item.price}</h2>
         </div>
 
         <button class="add-btn" data-id="${item.id}">+</button>
@@ -33,6 +34,8 @@ document.addEventListener('click', function(e) {
     if(e.target.dataset.id) {
         addToCart(e.target.dataset.id)
         console.log(e.target.dataset.id)
+    }else if(e.target.dataset.remove){
+        removeFromCart(e.target.dataset.remove)
     }
 })
 
@@ -42,6 +45,45 @@ function addToCart(itemId){
     })[0]
 
     cartArray.push(targetMenuObj)
+
+    renderOrder()
     console.log(cartArray)
+}
+
+function removeFromCart(itemId){
+   const targetIndex = cartArray.findIndex(function(item) {
+    return item.id == itemId
+})
+
+cartArray.splice(targetIndex, 1)
+
+renderOrder()
+    
+}
+
+
+function renderOrder() {
+    const orderHtml = cartArray.map(function(item) {
+        return `
+        <div class="order-item">
+            <h3>${item.name}</h3>
+
+            <p>$${item.price}</p>
+
+            <button class='remove-btn' data-remove='${item.id}'> remove </button>
+        </div>
+        `
+    }).join("")
+
+    const orderSum = cartArray.reduce((acc, currentItem) => {
+        return acc + currentItem.price;
+    }, 0)
+
+
+    orderContainer.innerHTML = `
+    <h2>Your Order</h2>
+    <p>Total Price: $${orderSum}</p>
+    ${orderHtml}
+    `
 }
 
