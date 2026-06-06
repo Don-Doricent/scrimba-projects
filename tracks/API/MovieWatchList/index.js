f3eb117916a500f81a026439f77c5edb7dc3350b
Don Doricent <107654196+Don-Doricent@users.noreply.github.com>
2026-06-06 12:40:38 -0400

    set up basic layout for watch list. Got search button to log input on click.

---
+++ tracks/API/MovieWatchList/index.js
@@ -0,0 +1,8 @@
+const searchEl = document.getElementById('searchButton')
+const inputEl = document.getElementById('movieInput')
+
+
+
+searchEl.addEventListener('click', function(event){
+    console.log(inputEl.value)
+})
