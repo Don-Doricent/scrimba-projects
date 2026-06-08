b1c6bfc2e32bc89919e3f4afd2a50cc9d5e5d43e
Don Doricent <107654196+Don-Doricent@users.noreply.github.com>
2026-06-07 21:01:05 -0400

    working through mapping and joining titles to main page after search.

--- tracks/API/MovieWatchList/index.js
+++ tracks/API/MovieWatchList/index.js
@@ -8,20 +8,42 @@ searchEl.addEventListener('click', function(event){
     fetch(`http://www.omdbapi.com/?apikey=a2e20d7f&s=${inputEl.value}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.Search[0].imdbID)
+           // console.log(data.Search[0].imdbID)
+
+           const movieNames = data.Search.map(movie => {
+                                return `
+                                ${movie.Title}
+                                ${movie.Year}
+                                
+                                `
+                            }).join(" ")
+            movieArea.innerHTML = movieNames
 
     fetch(`http://www.omdbapi.com/?apikey=a2e20d7f&i=${data.Search[0].imdbID}`)
         .then(res => res.json())
         .then(movieData => {
             console.log(movieData)
+           
 
-             movieArea.textContent = `
-                ${movieData.Title}
-                ${movieData.Poster}
-                ${movieData.Ratings[0]}
-                ${movieData.Year}
+             movieArea.innerHTML = `
+                <div class="movieCard">
+                    <img src="${movieData.Poster}" alt="${movieData.Title} poster">
 
+                    
+
+                    <div> 
+
+                        <h2>${movieData.Title}</h2>
+                        <p> ⭐ ${movieData.imdbRating}</p>
+                        <p>${movieData.Year}</p>
+                        <p>${movieData.Runtime}</p>
+                        <p>${movieData.Genre}</p>
+                        <p>${movieData.Plot}</p>
+                    </div>
+                <div>
              `
+
+              
         })
 
        
@@ -29,3 +51,7 @@ searchEl.addEventListener('click', function(event){
 
     
 })
+
+
+
+        
