9ce5efdbb5c72b0a5614887cb1d67a5cea2393df
Don Doricent <ddoricent97@gmail.com>
2026-07-30 11:36:22 -0400

    minor changes

--- digitalBusinessCard/DigitalBusinessCardNew/src/Body.jsx
+++ digitalBusinessCard/DigitalBusinessCardNew/src/Body.jsx
@@ -3,7 +3,7 @@ function Body(){
         <>
                 <div className="title">
                     <h1>Donald Doricent</h1>
-                    <h2>Fullstack Developer</h2>
+                    <h2>Frontend Developer</h2>
                     <h3>dondoricent.netlify.app</h3>
                 </div>
 
